a91c4b7cf61c24d873209ee63098d3b8d63eb61d
ale-0293 <martin.a.ac@outlook.com>
2025-04-03 01:06:32 -0600

    Commit inicial, estructura base

---
+++ src/main/java/com/apso/app/ApsoApplication.java
@@ -0,0 +1,13 @@
+package com.apso.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apso/app/ApsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apso.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
