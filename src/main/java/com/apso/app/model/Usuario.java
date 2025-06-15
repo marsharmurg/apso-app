faf2f029d96836f9515ec74f09e9bb70fce868a2
Marsharmur <mm220290@ues.edu.sv>
2025-06-15 12:08:35 -0600

    Fix: ajustes en entidades y servicio CSV para corregir compilación

--- src/main/java/com/apso/app/model/Usuario.java
+++ src/main/java/com/apso/app/model/Usuario.java
@@ -30,7 +30,6 @@ public class Usuario {
     @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<SorteoGrupal> sorteos;
 
-}
     private String sub;
 
 public void setSub(String sub) {
