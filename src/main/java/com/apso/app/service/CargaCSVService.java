1859b6a40baadf59552c499b421383af58bb54b1
ale-0293 <martin.a.ac@outlook.com>
2025-06-16 22:04:43 -0600

    Historial por usuario.

--- src/main/java/com/apso/app/service/CargaCSVService.java
+++ src/main/java/com/apso/app/service/CargaCSVService.java
@@ -40,8 +40,8 @@ public class CargaCSVService {
                 estudiante.setEmail(record.get("email"));
                 estudiante.setGrupoTeorico(record.get("grupo_teorico"));
                 estudiante.setAsignatura(record.get("asignatura"));
-                estudiante.setCargaId(Long.parseLong(record.get("carga_id")));
-                estudiante.setUsuario(usuarioActual); // vínculo directo al usuario
+                //estudiante.setCargaId(record.get("carga_id"));
+                estudiante.setCargaId(Integer.parseInt(record.get("carga_id")));
 
                 estudiantes.add(estudiante);
             }
